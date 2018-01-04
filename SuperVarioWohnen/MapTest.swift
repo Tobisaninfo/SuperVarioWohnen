df86242f514f63e2633b6bcbfbb00558f28ef897
s0553392 <s0553392@htw-berlin.de>
2018-01-04 17:42:29 +0100

    Route ready

--- SuperVarioWohnen/MapTest.swift
+++ SuperVarioWohnen/MapTest.swift
@@ -8,28 +8,28 @@
 
 import UIKit
 import MapKit
-class MapTest: UIViewController, CLLocationManagerDelegate,UISearchBarDelegate,MKMapViewDelegate {
+import CoreLocation
+class MapTest: UIViewController, CLLocationManagerDelegate,UISearchBarDelegate,MKMapViewDelegate,MKLocalSearchCompleterDelegate {
+
 
     @IBOutlet var naviCon: UINavigationItem!
-    
     let searchBar = UISearchBar()
-    @IBOutlet weak var route: UIBarButtonItem!
-    @IBOutlet weak var filter: UIBarButtonItem!
     @IBOutlet weak var mapView: MKMapView!
-    var myRoute : MKRoute!
-
+    var myLocation:CLLocation!
+    
+    var searchMode:Bool!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         enableLocationServices()
-        searchBar.placeholder="Placeholder"
+        searchBar.placeholder="Suchen..."
         naviCon?.titleView = searchBar
         searchBar.delegate = self
-        
-        
+        searchMode=true
  }
-
+    
+    
     let regionRadius: CLLocationDistance = 1000
     
     let locationManager = CLLocationManager()
@@ -56,34 +56,86 @@ class MapTest: UIViewController, CLLocationManagerDelegate,UISearchBarDelegate,M
         }
     }
     
-    @IBAction func makeRoute(_ sender: UIBarButtonItem) {
+
+    
+    @IBAction func route(_ sender: UIBarButtonItem) {
+        searchMode=false
+        searchBar.placeholder="Route..."
+    }
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
         
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBar.text
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        let pointAnnotation = MKPointAnnotation()
+        search.start { (response: MKLocalSearchResponse!, error: Error?) in
+           /* let items = response.mapItems
+
+            if(!self.searchMode!){
+                self.makeRoute(items: items)
+            }
+            else{
+            let placemarks = NSMutableArray()
+            
+            for item in items {
+                placemarks.add(item.placemark)
+            }
+            
+            self.mapView.showAnnotations(placemarks as! [MKAnnotation], animated: true)
+            }*/
+            
+
+            
+            pointAnnotation.title = searchBar.text
+            pointAnnotation.coordinate = CLLocationCoordinate2D(latitude:response!.boundingRegion.center.latitude, longitude: response!.boundingRegion.center.longitude)
+            self.mapView.addAnnotation(pointAnnotation)
+            if(self.searchMode){
+                self.mapView.setRegion(MKCoordinateRegionMake(pointAnnotation.coordinate, MKCoordinateSpanMake(0.2,0.2)), animated: true)
+            }
+            else{
+                self.routeShow(pointAnnotation: pointAnnotation)
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    func routeShow(pointAnnotation: MKPointAnnotation){
         
         let point1 = MKPointAnnotation()
-        let point2 = MKPointAnnotation()
+        let point2 = pointAnnotation
         
-        point1.coordinate = CLLocationCoordinate2DMake(25.0305, 121.5360)
-        point1.title = "Taipei"
-        point1.subtitle = "Taiwan"
-        mapView.addAnnotation(point1)
+        point1.coordinate = CLLocationCoordinate2DMake(myLocation.coordinate.latitude, myLocation.coordinate.longitude)
+        mapView.addAnnotation(pointAnnotation)
         
-        point2.coordinate = CLLocationCoordinate2DMake(24.9511, 121.2358)
-        point2.title = "Chungli"
-        point2.subtitle = "Taiwan"
-        mapView.addAnnotation(point2)
-        mapView.centerCoordinate = point2.coordinate
+        
         mapView.delegate=self
         
-        mapView.setRegion(MKCoordinateRegionMake(point2.coordinate, MKCoordinateSpanMake(0.7,0.7)), animated: true)
+        let coordinate1=CLLocation(latitude:point1.coordinate.latitude,longitude: point1.coordinate.longitude)
+        
+        let coordinate2=CLLocation(latitude:point2.coordinate.latitude,longitude: point2.coordinate.longitude)
+        
+        let region = coordinate1.distance(from: coordinate2)
+        
+        let diff1=(point1.coordinate.latitude+point2.coordinate.latitude)/2
+        let diff2=(point1.coordinate.longitude+point2.coordinate.longitude)/2
+        let test=CLLocation(latitude:diff1,longitude: diff2)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(test.coordinate,region, region)
+        mapView.setRegion(coordinateRegion, animated: true)
         
         let directionsRequest = MKDirectionsRequest()
         
-        let markTaipei = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point1.coordinate.latitude, point1.coordinate.longitude), addressDictionary: nil)
+        let source = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point1.coordinate.latitude, point1.coordinate.longitude), addressDictionary: nil)
         
-        let markChungli = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point2.coordinate.latitude, point2.coordinate.longitude), addressDictionary: nil)
+        let target = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point2.coordinate.latitude, point2.coordinate.longitude), addressDictionary: nil)
         
-        directionsRequest.source = MKMapItem(placemark: markChungli)
-        directionsRequest.destination = MKMapItem(placemark: markTaipei)
+        directionsRequest.source = MKMapItem(placemark: source)
+        directionsRequest.destination = MKMapItem(placemark: target)
         
         directionsRequest.transportType = MKDirectionsTransportType.automobile
         
@@ -96,23 +148,16 @@ class MapTest: UIViewController, CLLocationManagerDelegate,UISearchBarDelegate,M
             if error != nil {
                 print("Error getting directions")
             } else {
-                
                 for route in response!.routes {
-                    
-                    self.mapView.add(route.polyline,
-                                level: MKOverlayLevel.aboveRoads)
-                    for step in route.steps {
-                        print(step.instructions)
-                    }
+                    self.mapView.add(route.polyline, level: MKOverlayLevel.aboveRoads)
                 }
-                
             }
         })
-
+        searchMode=true
+        
+        searchBar.placeholder="Suchen..."
         
     }
-    
-    
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         
         
@@ -126,26 +171,6 @@ class MapTest: UIViewController, CLLocationManagerDelegate,UISearchBarDelegate,M
         
     }
     
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
-        let request = MKLocalSearchRequest()
-        request.naturalLanguageQuery = searchBar.text
-        request.region = mapView.region
-        let search = MKLocalSearch(request: request)
-        
-        
-        search.start { (response: MKLocalSearchResponse!, error: Error?) in
-            let items = response.mapItems
-            let placemarks = NSMutableArray()
-            
-            for item in items {
-                placemarks.add(item.placemark)
-            }
-            
-
-            self.mapView.showAnnotations(placemarks as! [MKAnnotation], animated: true)
-        }
-    }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         switch status {
@@ -165,6 +190,7 @@ class MapTest: UIViewController, CLLocationManagerDelegate,UISearchBarDelegate,M
         let userLocation:CLLocation = locations[0] as CLLocation
         manager.stopUpdatingLocation()
         centerMapOnLocation(location: userLocation)
+        myLocation=userLocation
     }
     
     func centerMapOnLocation(location: CLLocation) {
@@ -182,3 +208,4 @@ class MapTest: UIViewController, CLLocationManagerDelegate,UISearchBarDelegate,M
     
     
 }
+
