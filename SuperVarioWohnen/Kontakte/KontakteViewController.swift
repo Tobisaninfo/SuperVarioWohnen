e90d36f94cf9b3aff078011fad2ca2bfff636c4b
max.bause <max.bause@bonial.com>
2018-01-11 17:48:33 +0100

    Added KontakteViewController

---
+++ SuperVarioWohnen/Kontakte/KontakteViewController.swift
@@ -0,0 +1,27 @@
+//
+//  KontakteViewController.swift
+//  SuperVarioWohnen
+//
+//  Created by Max Bause on 11.01.18.
+//  Copyright © 2018 Tobias. All rights reserved.
+//
+
+import UIKit
+class KontakteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func call(_ sender: Any) {
+        let tel: URL = URL(string: "tel:/1028464")!
+        UIApplication.shared.open(tel, options: [:], completionHandler: nil);
+    }
+    
+}
