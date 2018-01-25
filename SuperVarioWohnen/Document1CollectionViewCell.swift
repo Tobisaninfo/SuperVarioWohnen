616e7783460e765f2337be0ddffcc08415156bbe
nikiema <tnowa2001@yahoo.fr>
2018-01-25 21:02:06 +0100

    Add missing files

---
+++ SuperVarioWohnen/Document1CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  Document1CollectionViewCell.swift
+//  collectonprojekt
+//
+//  Created by Mac on 14.12.17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class Document1CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
---
+++ SuperVarioWohnen/DokumentCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DokumentCollectionViewCell.swift
+//  collectonprojekt
+//
+//  Created by Mac on 30.11.17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class DokumentCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var documentimageview: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
