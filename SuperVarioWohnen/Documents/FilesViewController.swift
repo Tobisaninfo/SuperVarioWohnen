f991ebe1045c1d5888d3d701ef96c3fa8ad8266d
tobias <thinkdifferent055@gmail.com>
2018-01-26 11:26:33 +0100

    Cleanup everything

--- SuperVarioWohnen/Documents/FilesViewController.swift
+++ SuperVarioWohnen/Documents/FilesViewController.swift
@@ -11,21 +11,16 @@ import UIKit
 class FilesViewController: UIViewController,  UICollectionViewDataSource ,UICollectionViewDelegate {
     
     @IBOutlet weak var collectionview: UICollectionView!
-    var documentname = String ()
-    var foldername:String!
-    var pdfname:String!
-    var savepdfname : String!
-    var documentid : Int!
+    
+    var documents: [Document] = []
+    var selectedDocument: Int?
     
     let downloadFileURL = "https://thecodelabs.de:2530/documents/"
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         collectionview.dataSource = self
-        
-        self.downloadFile(Urlstring: downloadFileURL + String(documentid), pdfname: pdfname )
     }
     
     override func didReceiveMemoryWarning() {
@@ -36,71 +31,27 @@ class FilesViewController: UIViewController,  UICollectionViewDataSource ,UIColl
     // Mark  collectionView  Number of Cell
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return  1
+        return documents.count
     }
     
     // Mark  collectionViewcell
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ordner2", for: indexPath) as!  DocumentsViewCell
-        cell.label.text = pdfname
+        cell.label.text = documents[indexPath.row].name
         
         return cell
     }
     
-    // Mark Passing PDF name Data to  WebViewController
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.selectedDocument = indexPath.row
+        self.performSegue(withIdentifier: "webview", sender: self)
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if segue.identifier == "webview"
-        {
-            if self.collectionview.indexPath(for: (sender as? DocumentsViewCell)!) != nil
-            {
-                let viewVC = segue.destination as! WebViewController
-                print(viewVC)
-                viewVC.savepdfname =  pdfname+".pdf"
-                
-                
-                
-            }
+        if segue.identifier == "webview", let selectedDocument = selectedDocument {
+            let viewVC = segue.destination as! WebViewController
+            viewVC.document = documents[selectedDocument]
         }
-    }
-    
-    // Mark download pdf Document
-    func downloadFile( Urlstring: String, pdfname: String)  {
-        // Create destination URL
-        // let yourAuthorizationToken = "ztiuohijopk" // whatever is your token
-        //  request.setValue(yourAuthorizationToken, forHTTPHeaderField: "auth")
-        
-        let yourUrl = URL(string:Urlstring ) // whatever is your url
-        var request = URLRequest(url:yourUrl!)
-        let yourAuthorizationToken = "ztiuohijopk" // whatever is your token
-        request.setValue(yourAuthorizationToken, forHTTPHeaderField: "auth")
-        
-        request.setValue("application/pdf", forHTTPHeaderField: "Accept-Type")
-        let documentsUrl:URL =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first as URL!
-        let destinationFileUrl = documentsUrl.appendingPathComponent(pdfname+".pdf")
-        let sessionConfig = URLSessionConfiguration.default
-        let session = URLSession(configuration: sessionConfig)
-        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
-            if let tempLocalUrl = tempLocalUrl, error == nil {
-                // Success
-                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                    print("Status code: \(statusCode)")
-                }
-                
-                do {
-                    try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
-                } catch (let writeError) {
-                    print("Error creating a file \(destinationFileUrl) : \(writeError)")
-                }
-            } else {
-                print("Error took place while downloading a file. Error description: %@", error?.localizedDescription);
-            }
-        }
-        task.resume()
     }
 }
-
-
--- SuperVarioWohnen/Documents/WebViewController.swift
+++ SuperVarioWohnen/Documents/WebViewController.swift
@@ -12,32 +12,71 @@ import WebKit
 
 class WebViewController:  UIViewController{
     
-    @IBOutlet weak var webe: UIWebView!
-    //  var backgroundSession: URLSession!
-    var savepdfname:String!
-    var urlLink: URL!
+    @IBOutlet weak var webView: UIWebView!
+    
+    let token = "ztiuohijopk"
+    let urlString = "https://thecodelabs.de:2530/documents/"
+    
+    var document: Document?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(self.savepdfname)
-        loadPdf(pdfname:savepdfname )
+        
+        if let document = document {
+            downloadFile(urlString: urlString, document: document)
+        }
         setupViews()
     }
     
-    // Mark: - function to load Pdf data
-    func loadPdf(pdfname: String) {
-        
-        var pdfURL = (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)).last! as URL
-        pdfURL = pdfURL.appendingPathComponent(pdfname) as URL
-        
-        let data = try! Data(contentsOf: pdfURL)
-        self.webe.load(data, mimeType: "application/pdf", textEncodingName:"", baseURL: pdfURL.deletingLastPathComponent())
-        print(pdfURL)
+    // MARK: - download pdf Document
+    func downloadFile(urlString: String, document: Document)  {
+        if let url = URL(string: urlString.appending("\(document.id)")) {
+            var request = URLRequest(url: url)
+            
+            request.setValue(token, forHTTPHeaderField: "auth")
+            request.setValue("application/pdf", forHTTPHeaderField: "Accept-Type")
+            
+            let sessionConfig = URLSessionConfiguration.default
+            let session = URLSession(configuration: sessionConfig)
+            
+            let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+                if let tempLocalUrl = tempLocalUrl, error == nil {
+                    // Success
+                    if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                        print("Status code: \(statusCode)")
+                    }
+                    
+                    if let documentsUrl = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true) {
+                        let destinationFileUrl = documentsUrl.appendingPathComponent(document.name + ".pdf")
+                        do {
+                            try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+                        } catch (let writeError) {
+                            print("Error creating a file \(destinationFileUrl) : \(writeError)")
+                        }
+                        DispatchQueue.main.async {
+                            self.loadPdf(url: destinationFileUrl)
+                        }
+                    }
+                } else {
+                    if let error = error {
+                        print("Error took place while downloading a file. \(error.localizedDescription)")
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    // MARK: - function to load Pdf data
+    func loadPdf(url: URL) {
+        if let data = try? Data(contentsOf: url) {
+            self.webView.load(data, mimeType: "application/pdf", textEncodingName:"", baseURL: url)
+        }
     }
     
     // Mark: -Function to display Webview
     func setupViews() {
         view.backgroundColor = .white
-        view.addSubview(webe)
+        view.addSubview(webView)
     }
 }
