fafed3712e27fc2c7ac169c1d475423520bbf7db
max.bause <max.bause@bonial.com>
2018-01-28 01:22:41 +0100

    Merged QR-Scanner

--- SuperVarioWohnen/QR-Code/QRcodeController.swift
+++ SuperVarioWohnen/QR-Code/QRcodeController.swift
@@ -25,27 +25,27 @@ public protocol QRcodeControllerDelegate {
  An UIViewController for QRcode Recogition. This ViewController adds a ```AVCaptureVideoPreviewLayer``` on top of the view stack and starts an ```AVCaptureSession```.
  */
 public class QRcodeController: UIViewController {
-    
+
     private var captureSession = AVCaptureSession()
     private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    
+
     private let supportedCodeTypes = [AVMetadataObject.ObjectType.qr]
-    
+
     // MARK: - Delegate
-    
+
     /**
      Delegate for the QRcodeController.
      */
     public var delegate: QRcodeControllerDelegate?
-    
+
     // MARK: - Properties
-    
+
     /**
      Allow to recognize multiple codes at the same time.
      */
     public var allowMultipleMetadataObjects: Bool = false
-    
-    
+
+
     /**
      Enable the devices torch, if available.
      */
@@ -55,7 +55,7 @@ public class QRcodeController: UIViewController {
                 if captureDevice.hasFlash && captureDevice.hasTorch {
                     do {
                         try captureDevice.lockForConfiguration()
-                        
+
                         if isTorchEnable {
                             try captureDevice.setTorchModeOn(level: AVCaptureDevice.maxAvailableTorchLevel)
                         } else {
@@ -69,20 +69,20 @@ public class QRcodeController: UIViewController {
             }
         }
     }
-    
+
     // MARK: - ViewController
-    
+
     /**
      Is called, then the view is loaded.
      */
     override public func viewDidLoad() {
         super.viewDidLoad()
-        
+
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {
             print("Failed to get the camera device")
             return
         }
-        
+
         do {
             try captureDevice.lockForConfiguration()
             captureDevice.focusPointOfInterest = CGPoint(x: 0.5, y: 0.5)
@@ -92,56 +92,55 @@ public class QRcodeController: UIViewController {
         } catch {
             print("Fail to set autofocus: \(error)")
         }
-        
+
         do {
             // Get an instance of the AVCaptureDeviceInput class using the previous device object.
             let input = try AVCaptureDeviceInput(device: captureDevice)
-            
+
             // Set the input device on the capture session.
             captureSession.addInput(input)
-            
+
             // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
             let captureMetadataOutput = AVCaptureMetadataOutput()
             captureSession.addOutput(captureMetadataOutput)
-            
+
             // Set delegate and use the default dispatch queue to execute the call back
             captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
             //            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-            
+
         } catch {
             // If any error occurs, simply print it out and don't continue any more.
             print(error)
             return
         }
-        
+
         // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
         videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
         videoPreviewLayer?.frame = view.layer.bounds
         view.layer.addSublayer(videoPreviewLayer!)
-    }
-    
-    override public func viewWillAppear(_ animated: Bool) {
+
+        // Start video capture.
         captureSession.startRunning()
     }
-    
+
+    override public func viewWillAppear(_ animated: Bool) {
+        // Start video capture.
+    }
+
     override public func viewDidDisappear(_ animated: Bool) {
     }
-    
-    public override func viewWillDisappear(_ animated: Bool) {
-        captureSession.stopRunning()
-    }
-    
+
     override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
     private var processing: [String] = []
 }
 
 extension QRcodeController: AVCaptureMetadataOutputObjectsDelegate {
-    
+
     /**
      Handles the recognized QRcodes.
      */
@@ -150,10 +149,10 @@ extension QRcodeController: AVCaptureMetadataOutputObjectsDelegate {
         if metadataObjects.count == 0 {
             return
         }
-        
+
         // Get the metadata object.
         let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-        
+
         if supportedCodeTypes.contains(metadataObj.type) {
             let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
             if let code = metadataObj.stringValue, let bounds = barCodeObject?.bounds, !processing.contains(code) {
@@ -163,8 +162,8 @@ extension QRcodeController: AVCaptureMetadataOutputObjectsDelegate {
             }
         }
     }
-    
-    
+
+
     /**
      Mark QRcode as finish.
      - Parameter code: QRcode from the delegate method ```QRcodeControllerDelegate.QRcodeDidDetect(code:frame:)```
@@ -175,4 +174,3 @@ extension QRcodeController: AVCaptureMetadataOutputObjectsDelegate {
         }
     }
 }
-
