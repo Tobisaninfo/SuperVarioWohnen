dd9683141623e86d30a290995aae7e6ae9af17a1
tobias <thinkdifferent055@gmail.com>
2018-01-28 12:13:04 +0100

    Add basic board view; fixed qr code saving

--- SuperVarioWohnen/QR-Code/ScannerViewController.swift
+++ SuperVarioWohnen/QR-Code/ScannerViewController.swift
@@ -18,40 +18,47 @@ import UIKit
 import AVFoundation
 
 class ScannerViewController: QRcodeController, QRcodeControllerDelegate {
-
+    
     @IBOutlet weak var flashlightButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate = self
         view.bringSubview(toFront: flashlightButton)
     }
-
+    
     func QRcodeDidDetect(code: String, frame: CGRect) {
         AudioServicesPlayAlertSound(kSystemSoundID_Vibrate);
         DispatchQueue.global().async {
             self.checkCode(code: code)
         }
     }
-
+    
     func checkCode(code: String) -> Void {
         let url : URL = URL(string:"https://thecodelabs.de:2530/validation")!
         var request = URLRequest(url: url )
         request.setValue(code, forHTTPHeaderField: "auth")
-        URLSession.shared.dataTask(with: request) { data, response, error in
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
             if let response = response as? HTTPURLResponse {
+                print(response.statusCode)
                 if  response.statusCode == 200 {
                     self.writeCode(code: code)
                 }
             }
         }
+        task.resume()
     }
-
-    func writeCode(code : String){
-        let defaults = UserDefaults.standard
-        defaults.set(code, forKey: "qr")
+    
+    func writeCode(code : String) {
+        do {
+            let url = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("qr.txt")
+            try code.write(to: url, atomically: true, encoding: .utf8)
+        } catch {
+            print(error)
+        }
     }
-
+    
     @IBAction func flashlightHandler(_ sender: Any) {
         isTorchEnable = !isTorchEnable
         flashlightButton.setImage(isTorchEnable ? #imageLiteral(resourceName: "Flash-Filled") : #imageLiteral(resourceName: "Flash"), for: .normal)
